4bdb30ed6812a57b162ab8f5775232a09af8e341
PedroPereiradosSantos37 <rm552047@fiap.com.br>
2023-11-14 18:08:45 -0300

    Fazendo a estruturação da página com GRID.

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -7,12 +7,25 @@ export default function Home() {
     
     if(sessionStorage.getItem("token-user")){
     return (
-      <div>
-        <h1> Oi Home</h1>
-        <Cabecalho/>
+      <>
+      <Cabecalho/>
+      <div className="container">
+        <header>header1</header>
+        <nav>Nav4</nav>
+        <h1>H1</h1>
+        <h2>H2</h2>
+        <button>Mais informações</button>
       </div>
+      <h2>Vantagens:</h2>
+      <h4>Monitoramento 24 horas</h4>
+      <p>Monitoramento de oxigênio e batimentos cardíacos 24 horas</p>
+      <h4></h4>
+      <p></p>
+      <h4></h4>
+      <p></p>
+
+      </>
     )}else{
       window.location = "/login";
-      
     }
   } 
