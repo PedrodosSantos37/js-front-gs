23cd85fdd8386b868151634cc981f36c587449c6
PedroPereiradosSantos37 <rm552047@fiap.com.br>
2023-11-14 11:53:19 -0300

    Redirecionando o user do Login para a página Home.

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,11 +1,17 @@
 import "../../style/Home.scss"
 
-export default function Home() {
-    
-    return(
-        <>
-        <p>Oi Home</p>
-        </>
-    )
 
-}    
+export default function Home() {
+    document.title = "HOME";
+    
+    if(sessionStorage.getItem("token-user")){
+    return (
+      <div>
+        <h1> Oi Home</h1>
+  
+      </div>
+    )}else{
+      window.location = "/login";
+      
+    }
+  } 
